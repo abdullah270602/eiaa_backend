248fd3a4eca47a986244bbce498def92c962b40b
Abdullah Naseem <abdullahnaseem27@gmail.com>
2025-07-08 19:45:56 +0500

    Add metadata extraction for csv and xlsx

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -1,3 +1,4 @@
+import time
 from fastapi import APIRouter, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 import pandas as pd
@@ -5,12 +6,14 @@ import io
 from typing import List, Dict, Any
 import logging
 
+logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/upload", tags=["Upload"])
 
 @router.post("/",)
 async def upload(file: UploadFile = File(...)):
-    logging.info(f"Uploading file {file.filename}")
+    logger.info(f" Uploading file {file.filename}")
+    start = time.time()
     try:
         allowed_extensions = ['.csv', '.xlsx', '.xls']
         file_extension = None
@@ -28,18 +31,45 @@ async def upload(file: UploadFile = File(...)):
                 status_code=400, 
                 detail=f"Unsupported file type. Allowed types: {', '.join(allowed_extensions)}"
             )
-            
-
+        
+        metadata = None
+        
         if file_extension == '.csv':
-            response = "CSV"
-        else:
-            response = "XLS"
+
+
+            metadata = pd.read_csv(file.file, nrows=0)
             
-        return JSONResponse(content=response)
+            file.file.seek(0) # this a pointer to the beginning of the file
+            row_count = sum(1 for _ in file.file) - 1  # Subtract header row
+            file.file.seek(0)
+            
+            
+        else: # xlsx or xls
+
+            metadata = pd.read_excel(file.file, nrows=0)
+            
+            file.file.seek(0)
+            df_full = pd.read_excel(file.file, usecols=[0])  # Only load 1 column
+            row_count = df_full.shape[0]
+            file.file.seek(0)
+            
+            
+            
+        end_time = round(time.time() - start, 2)
+        columns = list(metadata.columns)
+        # dtypes = metadata.dtypes.apply(lambda x: str(x)).to_dict()
+
+        return JSONResponse(content={
+            "filename": file.filename,
+            "columns": columns,
+            # "dtypes": dtypes,
+            "num_rows": row_count,
+            "time_taken": end_time
+        })
     
     except HTTPException:
         raise
 
     except Exception as e:
-        logging.error(e)
+        logger.error(e)
         raise HTTPException(status_code=500, detail="Internal Server Error")
